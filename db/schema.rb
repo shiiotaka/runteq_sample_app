70990ac326e818811d789e841ebf0c0eeb4bab2b
shiiotaka <shiiotaka@gmail.com>
2021-01-28 23:03:43 +0900

    scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_01_28_134644) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
