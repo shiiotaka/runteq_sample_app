f19f3b273766860a16aaf3e9310009e3ddacdd1d
shiiotaka <shiiotaka@gmail.com>
2021-01-28 23:54:35 +0900

    model_create

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_134644) do
+ActiveRecord::Schema.define(version: 2021_01_28_145314) do
 
   create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
