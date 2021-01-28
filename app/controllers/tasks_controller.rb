bdb2ef598d89d2cc7981f96043b59d1895255c2d
shiiotaka <shiiotaka@gmail.com>
2021-01-29 00:47:11 +0900

    課題2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,55 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-  def idnex
+  def index
+    @tasks = Task.all
   end
 
   def show
+    # @task = Task.find(params[:id])
+  end
 
+  def new
+    @task = Task.new
   end
 
   def create
-
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: '(自作)記事を作成しました'
+    else
+      render 'new'
+    end
   end
 
   def edit
-
+    # @task = Task.find(params[:id])
   end
 
   def update
-
+    # @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @task, notice: '(自作)記事を更新しました'
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-    
+    # @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: '(自作)記事を削除しました'
+
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :body)
   end
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :title, presence: true, length: {in: 5..140}
+  validates :body, presence: true
 end
